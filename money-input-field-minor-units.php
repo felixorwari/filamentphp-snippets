c53262accd57cf422b091b5f2a127a109e6ac5d3
Felix Orwari <felix.orwari@gmail.com>
2026-06-26 10:45:22 +0300

    Add money input field for price and discounts

---
+++ money-input-field-minor-units.php
@@ -0,0 +1,64 @@
+<?php
+
+Select::make('price_currency')
+    ->label('Currency')
+    ->options([
+        'KES' => 'KES (KSh)',
+        'JPY' => 'JPY (¥)',
+        'USD' => 'USD ($)',
+    ])
+    ->default('KES')
+    ->live()
+    ->partiallyRenderComponentsAfterStateUpdated([
+        'price_amount',
+        'discount_price_amount',
+    ])
+    ->required()
+    ->columnSpan(1),
+
+// Original price, usually displayed as crossed out
+TextInput::make('price_minor_units')
+    ->label('Price')
+    ->prefix(fn (Get $get): ?string => $get('price_currency')) // update with selected currency
+    ->placeholder('e.g. 1500000')
+    ->formatStateUsing(
+        fn (?int $state): ?float => $state !== null
+            ? $state / 100
+            : null
+    ) // display amount in major units
+    ->dehydrateStateUsing(
+        fn ($state) => $state !== null
+            ? (int) round($state * 100)
+            : null
+    ) // persist amount in minor units
+    ->required()
+    ->minValue(0)
+    ->numeric()
+    ->columnSpan([
+        'default' => 1,
+        'lg' => 2,
+    ]),
+
+// Original price, usually displayed as crossed out
+// Suitable for when discounts are available, or pricees changed
+TextInput::make('original_price_minor_units')
+    ->label('Discounted price')
+    ->prefix(fn (Get $get): ?string => $get('price_currency'))
+    ->helperText('If a discount price is set, the actual price will be displayed as struck through.')
+    ->placeholder('e.g. 1400000')
+    ->formatStateUsing(
+        fn (?int $state): ?float => $state !== null
+            ? $state / 100
+            : null
+    ) // display amount in major units
+    ->dehydrateStateUsing(
+        fn ($state) => $state !== null
+            ? (int) round($state * 100)
+            : null
+    ) // persist amount in minor units
+    ->minValue(0)
+    ->numeric()
+    ->columnSpan([
+        'default' => 1,
+        'lg' => 2,
+    ]),
